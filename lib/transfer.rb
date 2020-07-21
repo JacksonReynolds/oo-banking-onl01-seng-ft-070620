6cb8114ad99ebafaa5f73d7631f789cf3e572020
Jackson Reynolds <jwreynold@gmail.com>
2020-07-21 01:10:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,12 @@
+require 'bank_account.rb'
+
 class Transfer
-  # your code here
+
+  def initialize(sender, receiver, amt)
+    @sender = sender
+    @receiver = receiver
+    @status = pending
+    @amount = amt
+  end #initialize
+
 end
