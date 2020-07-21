a5f4f4a308005cc594b9414e18f5010115823e90
Jackson Reynolds <jwreynold@gmail.com>
2020-07-21 01:01:58 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,5 +1,14 @@
 require 'transfer.rb'
 
 class BankAccount
+  
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = 'open'
+  end #initialize
 
 end
